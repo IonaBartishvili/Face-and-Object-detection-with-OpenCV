3e5e9e56578c077d1efdf3e075692b5f3220664c
SoulOfBelial <iona.bartishvili@gmail.com>
2019-11-29 22:12:55 +0400

    objectDet

--- Realtime Object Detection(Banana).py
+++ Realtime Object Detection(Banana).py
@@ -4,23 +4,23 @@ import cv2
 
 
 def draw_rectangle(img, classifier, scale, minNeighbour, text, color, font_family, thickness):
-    faces = classifier.detectMultiScale(img, scale, minNeighbour)
+    bananas = classifier.detectMultiScale(img, scale, minNeighbour)
     # x y w h are coordinates of face.
-    for (x, y, w, h) in faces:
-        # Drawing The Rectangle
+    for (x, y, w, h) in bananas:
+        # Drawing rectangle
         #           Starting point  End point
         #                    |         |
         cv2.rectangle(img, (x,y), (x+w, y+h), color, thickness)
-        #  Writing The text
+        # Writing The text
         #  Starting Point (where to start typing the text)
         #                        |
-        cv2.putText(img, text, (x,y-10), font_family, scale, color, thickness)
+        cv2.putText(img, text, (x,y), font_family, scale, color, thickness)
 
     return img
 
 
 # Initializing Face cascade with XML file from GIT
-faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+faceCascade = cv2.CascadeClassifier('BananaCascade.xml')
 # Capturing the Live stream video
 video_capture = cv2.VideoCapture(0)  # 0 for built-in's and -1 for external video cameras
 
@@ -28,7 +28,7 @@ while True:
     # reading Frames from the video
     _, img = video_capture.read()
     # Calling the method that detects face on frame and draws rectangle on it
-    draw_rectangle(img, faceCascade, 1.1, 10, 'Face', (255,0,0), cv2.FONT_HERSHEY_SIMPLEX, 2)
+    draw_rectangle(img, faceCascade, 1.1, 10, 'Banana', (0,255,0), cv2.FONT_HERSHEY_SIMPLEX, 2)
     # Showing video process in the new window
     cv2.imshow("", img)
     # Making whether user types 'q' to quit from the program
